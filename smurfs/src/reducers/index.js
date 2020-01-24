f8a8c9d49f2fc1db1f3e73c69b573882f0e0a48d
jDanielDetes <jdanieldetes@gmail.com>
2020-01-24 12:02:51 -0500

    actions loaded

--- smurfs/src/reducers/index.js
+++ smurfs/src/reducers/index.js
@@ -1,7 +1,18 @@
+import { FETCHING_ACTIVITY_SUCCESS } from "../components/actions"
 
 const initialState={
     name:"",
     age:"",
     height:"",
     error:""
+}
+
+export const reducer=(state= initialState,action) =>{
+    switch(action.type){
+        case FETCHING_ACTIVITY_SUCCESS:
+            return {
+                ...state,name:action.payload
+            }
+            default: return state 
+    }
 }
